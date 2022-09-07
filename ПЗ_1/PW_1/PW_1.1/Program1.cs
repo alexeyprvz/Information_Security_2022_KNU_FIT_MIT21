1f6d3dc08f0e9e4d921a0770e400f946b2a0f48a
Alexey Pereverzev <alexeyprvz@knu.ua>
2022-09-08 01:20:06 +0300

    PW_1

---
+++ ПЗ_1/PW_1/PW_1.1/Program1.cs
@@ -0,0 +1,7 @@
+﻿var rnd = new Random(335);
+
+for (int i = 5; i < 10; i++)
+{
+    var nrnd = rnd.Next(0, 10);
+    Console.WriteLine(nrnd);
+}
---
+++ ПЗ_1/PW_1/PW_1.2/Program2.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+
+var rnd = new RNGCryptoServiceProvider();
+var rndNum = new byte[10];
+rnd.GetBytes(rndNum);
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(rndNum[i]);
+}
