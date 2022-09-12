68dc7dd6788654504b0eadf32b665d8d7c17e6cd
alexeyprvz <alexeyprvz@knu.ua>
2022-09-12 21:25:38 +0300

    Add Crypto Key

--- ПЗ_1/PW_1/PW_1.2/Program2.cs
+++ ПЗ_1/PW_1/PW_1.2/Program2.cs
@@ -6,8 +6,13 @@ int length = Convert.ToInt32(Console.ReadLine());
 var rnd = new RNGCryptoServiceProvider();
 var rndNum = new byte[length];
 rnd.GetBytes(rndNum);
+string cryptoKey = Convert.ToBase64String(rndNum);
 
+Console.Write("\nBytes: ");
 for (int i = 0; i < length; i++)
 {
     Console.Write(rndNum[i]);
-}
+};
+
+Console.Write("\n\nText: ");
+Console.WriteLine(cryptoKey);
