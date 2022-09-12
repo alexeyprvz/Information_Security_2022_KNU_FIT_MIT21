aff494906eb38f85ef6b1e635aeccfa391214cd2
alexeyprvz <alexeyprvz@knu.ua>
2022-09-12 20:14:44 +0300

    Done solutions

--- ПЗ_1/PW_1/PW_1.1/Program1.cs
+++ ПЗ_1/PW_1/PW_1.1/Program1.cs
@@ -1,6 +1,6 @@
 ﻿var rnd = new Random(335);
 
-for (int i = 5; i < 10; i++)
+for (int i = 0; i < 10; i++)
 {
     var nrnd = rnd.Next(0, 10);
     Console.WriteLine(nrnd);
--- ПЗ_1/PW_1/PW_1.2/Program2.cs
+++ ПЗ_1/PW_1/PW_1.2/Program2.cs
@@ -1,9 +1,13 @@
 ﻿using System.Security.Cryptography;
 
+Console.Write("Enter lenght: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
 var rnd = new RNGCryptoServiceProvider();
-var rndNum = new byte[10];
+var rndNum = new byte[length];
 rnd.GetBytes(rndNum);
-for (int i = 0; i < 10; i++)
+
+for (int i = 0; i < length; i++)
 {
-    Console.WriteLine(rndNum[i]);
+    Console.Write(rndNum[i]);
 }
