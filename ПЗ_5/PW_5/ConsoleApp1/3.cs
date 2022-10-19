89632a1d127942c82771b942a68051bee26a8259
alexeyprvz <alexeyprvz@knu.ua>
2022-10-19 10:29:05 +0300

    3, 4

---
+++ ПЗ_5/PW_5/ConsoleApp1/3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PW_5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine(); 
+            byte[] salt = SaltedHash.GenerateSalt();
+            Console.WriteLine("\nSalt: " + Convert.ToBase64String(salt));
+            var hashedPassword = SaltedHash.HashPasswordWithSalt(Encoding.UTF8.GetBytes(password), salt);
+            Console.WriteLine("Hashed Password: " + Convert.ToBase64String(hashedPassword));
+        }
+    }
+    public class SaltedHash
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var rndnumgen = new RNGCryptoServiceProvider())
+            {
+                var rndnum = new byte[32]; 
+                rndnumgen.GetBytes(rndnum);
+                return rndnum;
+            }
+        }
+
+        private static byte[] Combine(byte[] first, byte[] second)
+        {
+            var ret = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+            return ret;
+        }
+
+        public static byte[] HashPasswordWithSalt(byte[] toBeHashed, byte[] salt) 
+        { 
+            using (var sha256 = SHA256.Create()) 
+            { 
+                return sha256.ComputeHash(Combine(toBeHashed, salt)); 
+            } 
+        }
+    }
+}
---
+++ ПЗ_5/PW_5/PW_5.4/4.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace PW_5._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            Console.Write("\n1 - MD5" +
+                "\n2 - SHA1" +
+                "\n3 - SHA256" +
+                "\n4 - SHA284" +
+                "\n5 - SHA512" +
+                "\n -> ");
+            int alg_chs = Convert.ToInt32(Console.ReadLine());
+
+            HashAlgorithmName alg;
+            if (alg_chs == 1)
+                alg = HashAlgorithmName.MD5;
+            if (alg_chs == 2)
+                alg = HashAlgorithmName.SHA1;
+            if (alg_chs == 3)
+                alg = HashAlgorithmName.SHA256;
+            if (alg_chs == 4)
+                alg = HashAlgorithmName.SHA384;
+            if (alg_chs == 5)
+                alg = HashAlgorithmName.SHA512;
+
+            int iter_step = 50000;
+            int cnt = 0;
+            byte[] salt;
+
+            while(cnt != 10)
+            {
+                var sw = new Stopwatch();
+                salt = PBKDF2.GenerateSalt();
+                sw.Start();
+                string hashedPassword = Convert.ToBase64String(PBKDF2.HashPassword(Encoding.UTF8.GetBytes(password), salt,(cnt + 1) * iter_step, alg));
+                sw.Stop();
+
+                Console.WriteLine("Hashed password: " + hashedPassword + "    Salt: " + Convert.ToBase64String(salt) +
+                    "     Iterations: " + ((cnt + 1) * iter_step) + "    Time: " + sw.ElapsedMilliseconds);
+                cnt++;
+                sw.Reset();
+            }
+            
+        }
+
+    }
+
+    public class PBKDF2
+    {
+        public static byte[] GenerateSalt()
+        {
+            using (var rndnumgen = new RNGCryptoServiceProvider())
+            {
+                var rndnum = new byte[32];
+                rndnumgen.GetBytes(rndnum);
+                return rndnum;
+            }
+        }
+        public static byte[] HashPassword(byte[] toBeHashed, byte[] salt, int numberOfRounds, HashAlgorithmName alg) {
+
+            using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, salt, numberOfRounds, alg)) { 
+                return rfc2898.GetBytes(20); 
+            } 
+        }
+    }
+}
